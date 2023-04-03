560a4d9df4878379b267990ed84aff50b629bf0c
Jolie Gielchinsky <jolieschae@gmail.com>
2023-04-03 15:23:56 -0400

    test

--- src/index.js
+++ src/index.js
@@ -1,52 +1,67 @@
-const menu = document.getElementById("#ramen-menu")
-const menuDetails = document.getElementById("#ramen-detail")
-const ratingDisplay = document.getElementById("#rating-display")
-const commentDisplay = document.getElementById("#comment-display")
-const form = document.getElementById("#new-ramen")
+let wildPkmn
+let party
+// party pokemon needs review to prevent duplicates
+const pokeballs = document.querySelectorAll(".partypkmn")
+let activePkmnImg = document.getElementById("activepkmnimg")
+let wildPkmnImg = document.getElementById("wildpkmnimg")
+let form = document.getElementById("battleform")
+let move1 = document.getElementById("move1")
+let move2 = document.getElementById("move2")
+let move3 = document.getElementById("move3")
+let move4 = document.getElementById("move4")
+let h1 = document.createElement("h1")
+let displayDiv = document.querySelector(".div-block")
+displayDiv.append(h1)
 
-fetch("http://localhost:3000/ramens") 
+fetch("http://localhost:3000/pokemon") 
 .then((response) => response.json())
 .then((data) => {
-    console.log(data) 
-loadRamen(data); 
+    wildPkmn = data[Math.floor(Math.random() * data.length)]
+    party = []
+    for (let i = 0; i < 6; i++) {
+        randomParty = data[Math.floor(Math.random() * data.length)];
+        party.push(randomParty);
+        }
+        wildPkmnImg.src = wildPkmn.image1
 });
 
-function loadRamen(ramens) {
-    ramens.forEach(function(ramens){  
-        let img = document.createNewElement("img")
-        img.src = ramens.img
-        menu.append(img);
-        img.addEventListener('click', () => {
-            
-            
-        });
-        }    
-    )};
-        menuDetails.h2.append(ramens.name); 
-        menuDetails.h3.append(ramens.restaurant); 
-        ratingDisplay.append(ramens.rating);
-        commentDisplay.append(ramens.comment);
+
+let activePkmn
+
+pokeballs.forEach((pokeball, index)=> {
+    pokeball.addEventListener("click", () => {
+        activePkmn = party[index]
+        activePkmnImg.src = activePkmn.image2
+        move1.textContent = activePkmn.moveset[0].name
+        move2.textContent = activePkmn.moveset[1].name
+        move3.textContent = activePkmn.moveset[2].name
+        move4.textContent = activePkmn.moveset[3].name
+
+    console.log(activePkmn)
+    });
+    pokeball.addEventListener("mouseover", () => {
+        h1.textContent = party[index].name
+    pokeball.setAttribute('data-pokemon', party[index].name);
+    });
+});
+
+
 
 form.addEventListener("submit", (e) => {
-    let newRamen = {}
-    e.preventDefault();
-        newRamen = {
-            name: e.target["ramen-name"].value,
-            restaurant: e.target.restaurant.value,
-            image: e.target.image.value,
-            rating: e.target.rating.value,
-            comment: e.target.comment.value,
-        };
-        postRamen(newRamen)
+    e.preventDefault()
+    
+
+
+
 });
-function postRamen(ramens) {
-    fetch("http://localhost:3000/ramens", {
-    method: "POST",
-    headers: {
-        "content-type": "Application/json",
-    },
-    body: JSON.stringify(ramens),
-})
-    .then((res) => res.json())
-    .then((data) => renderRamen([data]))
-}
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/readme.md
@@ -0,0 +1,5 @@
+https://www.w3schools.com/jsref/event_onmouseover.asp
+https://developer.mozilla.org/en-US/docs/Web/API/Element/setAttribute
+https://gamerant.com/pokemon-damage-calculation-help-guide/
+https://www.pkparaiso.com/pokedex/
+https://bulbapedia.bulbagarden.net/wiki/Main_Page
