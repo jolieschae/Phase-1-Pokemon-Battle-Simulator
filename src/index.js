eb4e0acb7836e0196471aaefcd48dbbd25f4997e
Jolie Gielchinsky <jolieschae@gmail.com>
2023-06-12 14:30:26 -0400

    updated readme

--- src/index.js
+++ src/index.js
@@ -17,36 +17,35 @@ let wildHealth = document.getElementById("wildhealth")
 fetch("http://localhost:3000/pokemon") 
 .then((response) => response.json())
 .then((data) => {
-    wildPkmn = data[Math.floor(Math.random() * data.length)]
-    party = []
+    wildPkmn = data[Math.floor(Math.random() * data.length)];
+    party = [];
     for (let i = 0; i < 6; i++) {
         randomParty = data[Math.floor(Math.random() * data.length)];
         party.push(randomParty);
-        }
-        wildPkmnImg.src = wildPkmn.image1
+    };
+        wildPkmnImg.src = wildPkmn.image1;
         wildHealth.max = wildPkmn.stats.hp;
 });
-
 
 let activePkmn
 
 pokeballs.forEach((pokeball, index)=> {
     pokeball.addEventListener("click", () => {
-        activePkmn = party[index]
-        activePkmnImg.src = activePkmn.image2
+        activePkmn = party[index];
+        activePkmnImg.src = activePkmn.image2;
         activeHealth.max = activePkmn.stats.hp; 
-        move1.textContent = activePkmn.moveset[0].name
-        move2.textContent = activePkmn.moveset[1].name
-        move3.textContent = activePkmn.moveset[2].name
-        move4.textContent = activePkmn.moveset[3].name
+        move1.textContent = activePkmn.moveset[0].name;
+        move2.textContent = activePkmn.moveset[1].name;
+        move3.textContent = activePkmn.moveset[2].name;
+        move4.textContent = activePkmn.moveset[3].name;
     });
     pokeball.addEventListener("mouseover", () => {
-        h1.textContent = party[index].name
+        h1.textContent = party[index].name;
     pokeball.setAttribute('data-pokemon', party[index].name);
     });
 
     pokeball.addEventListener("mouseout", () => {
-        h1.textContent = ""
+        h1.textContent = "";
     });
 });
 
@@ -59,43 +58,39 @@ function calculateDamage(attacker, defender, move) {
     const immunity = defender.immunity[move.type] || 1;
     const damage = Math.floor((((2 * 50) / 5 + 2) * move.power * (attackStat / defenseStat)) / 50 + 2);
     let finalDamage = Math.floor(damage * stab * effectiveness * resistances * immunity);
-    return finalDamage
-    }
+    return finalDamage;
+};
 
 let attack
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault()
-    let moveIndex = e.target.field.value
-    attack = activePkmn.moveset[moveIndex]
-    let randomIndex = Math.ceil(Math.random() * 3)
-    wildPkmnAttack = wildPkmn.moveset[randomIndex]
-    wildHealth.value -= calculateDamage(activePkmn, wildPkmn, attack)
+    e.preventDefault();
+    let moveIndex = e.target.field.value;
+    attack = activePkmn.moveset[moveIndex];
+    let randomIndex = Math.ceil(Math.random() * 3);
+    wildPkmnAttack = wildPkmn.moveset[randomIndex];
+    wildHealth.value -= calculateDamage(activePkmn, wildPkmn, attack);
     if (wildHealth.value > 0){
-    alert(`The wild ${wildPkmn.name} used ${wildPkmnAttack.name}!`)}
-    activeHealth.value -= calculateDamage(wildPkmn, activePkmn, wildPkmnAttack)
+    alert(`The wild ${wildPkmn.name} used ${wildPkmnAttack.name}!`)};
+    activeHealth.value -= calculateDamage(wildPkmn, activePkmn, wildPkmnAttack);
     if (wildHealth.value <= (wildHealth.max * .25) && wildHealth.value > 0){
-        alert(`The wild ${wildPkmn.name} is weak! Use the spacebar to throw a pokeball at it!`)
+        alert(`The wild ${wildPkmn.name} is weak! Use the spacebar to throw a pokeball at it!`);
         document.addEventListener("keydown", (event) => {
             if (event.code === "Space") {
-                wildPkmnImg.src = "https://s2.gifyu.com/images/pokeball.gif", wildPkmnImg.style.height = "29px", wildPkmnImg.style.top = "135px", wildPkmnImg.style.left = "340px"
+                wildPkmnImg.src = "https://s2.gifyu.com/images/pokeball.gif", wildPkmnImg.style.height = "29px", wildPkmnImg.style.top = "135px", wildPkmnImg.style.left = "340px";
                 window.alert(`You caught the wild ${wildPkmn.name}! ${wildPkmn.name} has been sent to your PC.`);
             };
-          });
-    }
+        });
+    };
     if (wildHealth.value === 0){
-        alert(`The wild ${wildPkmn.name} fainted!`)
-        window.location.reload()
+        alert(`The wild ${wildPkmn.name} fainted!`);
+        window.location.reload();
     }
     else if (activeHealth.value === 0){
-        alert(`Oh, no! Your ${activePkmn.name} fainted!`)
-        window.location.reload()
-    }
+        alert(`Oh, no! Your ${activePkmn.name} fainted!`);
+        window.location.reload();
+    };
 });
-
-// alerts when wild pkmn moves are launched
-
-// battle music playing on page
 
 
 
--- src/readme.md
+++ src/readme.md
@@ -1,3 +1,16 @@
+Welcome to our Phase 1 project! Our very first project in our journey through web development at the Flatiron School! This project was designed to be a demonstration of our knowledge of core, Javascript fundamentals put to use! 
+
+This is a simple Wild Pokemon Encounter simulation, akin to the real games, built on vanilla Javascript, HTML, and CSS. Upon opening the app, the user is faced with a wild Pokemon, and the goal is to catch it. Create a strategy using the 6 pokemon randomly assigned to your party, and choose one to send to battle. No 2 monsters are alike, and each type of Pokemon has their own game data, stats, typing, and movesets. 
+
+Pokemon is like one big game of rock, paper, scissors. Water hits fire harder (water puts out fire), fire hits grass harder (fire burns grass), and grass hits water harder (grass absorbs water). Pokemon take turns using different attack moves against each other. With 18 different types of Pokemon, battles strategies become dynamic and can get quite interesting. 
+
+The highlight of this project was a dynamic damage calculator that we created. By using ternaries to check the information in the database, the damage is calculated based on a variety of factors. It factors in a Pokemon's health points, their typing, the type of move they are using, the power that move possesses, and the Pokemon's stats. In the Pokemon franchise, moves can either be Special or Physical (an attack that uses some supernatural power to attack at a distance, or an attack that makes physical contact with the opponent), and will calculate damage based on a Pokemon's Special Attack stat or plain Attack stat, respectively. The damage is also calculated with either the defending Pokemon's Special Defense or plain Defense stat. Pokemon who use a move in battle that is the same as their own type get a 50% same-type-attack-boost, meaning a Flying type Pokemon using a Flying type move will deal more damage to their opponent than a Fire type using a Flying type move. Some Pokemon are completely immune to moves of particular typings! Ground type Pokemon take no damage against Electric types.
+
+When a wild Pokemon's health is below 25% of their total health points, the player can throw a pokeball to catch the Pokemon via a press of the spacebar. Knock the Pokemon out with a super strong attack before you can throw a ball, and you're out of luck! The Pokemon faints and disappears. Watch out - the wild Pokemon could knock your own Pokemon out too before you get the chance.
+
+We created a database of 66 fan-favorites for you to play with and catch. Try to catch them all!
+
+
 https://www.w3schools.com/jsref/event_onmouseover.asp
 https://developer.mozilla.org/en-US/docs/Web/API/Element/setAttribute
 https://gamerant.com/pokemon-damage-calculation-help-guide/
